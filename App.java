9499e84d953a6f08a2757a88d4d49c4a3f46f786
leviskhfvsdf <martinln@limestone.on.ca>
2025-11-26 13:06:09 -0500

    mapping for grid display
    mapped the distance and angle so it could fit in the grid i made.

--- App.java
+++ App.java
@@ -1,11 +1,19 @@
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.util.Scanner;
 import com.fazecast.jSerialComm.SerialPort;
+
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -14,25 +22,44 @@ public class App extends Application {
 
     public void start(Stage stage) throws InterruptedException {
 
-        SerialPort port = SerialPort.getCommPort("COM10");
+        SerialPort port = SerialPort.getCommPort("COM13");
         port.setBaudRate(9600);
         port.openPort();
 
-        if (!port.openPort()) {
-            System.out.println("Failed to open port!");
-            return;
-        }
-        System.out.println("Opened port!");
+        GridPane gridLayout = new GridPane();
 
+        StackPane stack = new StackPane();
+
+        Button grid[][] = new Button[5][5];
+        for (int y = 0;y < grid.length; y++)
+         for (int x = 0; x < grid.length; x++) {
+       
+                Button button = new Button();
+                grid[y][x] = button;
+                gridLayout.add(button, y, x);
+                button.setOpacity(0.7);
+                button.setPrefSize(20,20);
+                
+            
+         }
+        
         Scanner input = new Scanner(port.getInputStream());
         
-        Label distanceLabel = new Label("distance:   ");
-        // Create components to add.
-        VBox contentBox = new VBox();
+        Label distanceLabel = new Label();
 
-        contentBox.getChildren().addAll(distanceLabel);
-        // Set up the window and display it.
-        Scene scene = new Scene(contentBox, 300, 200);
+        Label angleLabel = new Label();
+        
+        Image img = new Image("file:C:\\Users\\martinln\\OneDrive - Limestone DSB\\computer coding class g11\\unit 2\\gui-mini-project-arrays-methods-file-i-o-leviskhfvsdf\\creatingsemicircle2013-06.png");
+        ImageView imageView = new ImageView(img);
+        imageView.setFitWidth(200);
+        imageView.setPreserveRatio(true);
+
+        VBox contentBox = new VBox(10);
+
+        stack.getChildren().addAll(imageView, gridLayout);
+        contentBox.getChildren().addAll(distanceLabel, angleLabel, stack);
+
+        Scene scene = new Scene(contentBox, 400, 400);
         stage.setScene(scene);
         stage.setTitle("Radar");
         stage.show();
@@ -40,10 +67,10 @@ public class App extends Application {
 
         Thread serialThread = new Thread(() -> {
 
-           
+        
             while (true) {
                 try {   
-                        Thread.sleep(1500);
+                        Thread.sleep(2000);
                         
                         String distance = input.nextLine();
                         String angle = input.nextLine();
@@ -55,7 +82,16 @@ public class App extends Application {
                         // UI update must be done on JavaFX Application Thread
                         Platform.runLater(() -> {
                             distanceLabel.setText("Distance: " + distance);
+                            angleLabel.setText("Angel: " + angle);
                         });
+
+                        double mappedDistance = MathUtils.scale(distance, 0, 200, 0, 5);
+                        Double mappedAngle = MathUtils.scale(angle, 0, 200, 0, 5);
+                        
+                        int Distance = (int) Math.round(mappedDistance);
+                        int Angle = (int) Math.round(mappedAngle);
+
+                        grid[Angle][Distance].setStyle("-fx-background-color: #000000ff;");
 
                 } catch (Exception e) {
                     e.printStackTrace();
---
+++ MathUtils.java
@@ -0,0 +1,9 @@
+
+public class MathUtils {
+
+    public static double scale(String distance, int i, int j, int k, int l) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'scale'");
+    }
+
+}
