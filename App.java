61e2abcf20278eb2ced8a48b232651193ea524ee
leviskhfvsdf <martinln@limestone.on.ca>
2025-12-04 12:18:40 -0500

    fixed scaling for image and grid
    removed more magic numbers

--- App.java
+++ App.java
@@ -22,9 +22,11 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    final static int gridCol = 10;  // dimentions of grid
+    final static int radarSize = 200; // dimention of grid and image {cant go past 200}
+    final static int gridCol = 10;  // amount of cells
     final static int gridRow = 10;
     final static String userSerialPort = "COM10";   // serial port
+
     Double rawDistance = 0.0;
     int rawAngle = 0;
 
@@ -66,22 +68,26 @@ public class App extends Application {
 
         Image semi = new Image("/semiCircle.png");  //background image for grid
         ImageView semiCircle = new ImageView(semi);
-        semiCircle.setFitWidth(200);
+        semiCircle.setFitWidth(radarSize);
+        semiCircle.setFitHeight(radarSize / 2); // hight should be half of width 
         semiCircle.setPreserveRatio(true);
+       
 
         Image bar = new Image("/blackbox.jpeg");   // rotating bar for image
         ImageView displayBar = new ImageView(bar);
         displayBar.setFitWidth(5);
-        displayBar.setFitHeight(100);
+        displayBar.setFitHeight(radarSize / 2);
         Rotate rotate = new Rotate();
         rotate.setPivotX(displayBar.getFitWidth());    // sets pivot point to the top of the bar         
         rotate.setAngle(90);  // initial rotation angle
         displayBar.getTransforms().add(rotate);
 
+        imageStack.getChildren().addAll(semiCircle, displayBar); // stackPane
+        imageStack.setAlignment(Pos.CENTER);
         
         Pane grid[][] = new Pane[gridCol][gridRow];
         gridLayout.prefWidthProperty().bind(semiCircle.fitWidthProperty());
-        gridLayout.prefHeightProperty().bind(semiCircle.fitWidthProperty().multiply(0.5));
+        gridLayout.prefHeightProperty().bind(semiCircle.fitHeightProperty()); // gridlayouts hight should be imgages hight
 
         for (int y = 0; y < grid.length; y++) { // loop for 2d grid array. goes through each cell
             for (int x = 0; x < grid[0].length; x++) {
@@ -92,15 +98,15 @@ public class App extends Application {
                 tile.setStyle("-fx-background-color: #29302fff;");
                 tile.setOpacity(0.3);
                 
-                tile.prefWidthProperty().bind(gridLayout.prefWidthProperty().divide(gridCol));
+                tile.prefWidthProperty().bind(gridLayout.prefWidthProperty().divide(gridCol)); // tile should be gridLayouts width divided by amount of collumns
                 tile.prefHeightProperty().bind(gridLayout.prefHeightProperty().divide(gridRow));
             }
         }
+        gridLayout.setAlignment(Pos.CENTER); // align grid with image
 
         Label distanceLabel = new Label("Distance:");
         Label angleLabel = new Label("Angle:");
 
-        imageStack.getChildren().addAll(semiCircle, displayBar); // stackPane
         gridAndImage.getChildren().addAll(imageStack, gridLayout); // stackPane
         gridAndImage.setMaxHeight(100);
         gridAndImage.setMinHeight(100);
